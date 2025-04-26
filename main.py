189f9998ba0efca5bafef24d3b05824c7dc1e2d1
AlwaysNur <info@nursplace.dev>
2025-04-26 12:05:54 +1200

    final

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import os
 import subprocess
 import sys
+import time
+from subprocess import DEVNULL
 
 path = sys.argv[1].strip()
-# out = sys.argv[2].strip()
+out = sys.argv[2].strip()
+zip_name = "/tmp/temp.zip"
 
-# print(path+ "11"+out)
-# print(sys.argv)
-subprocess.run(["zip", "temp"] + path,shell=True)
+subprocess.run(["zip","-j", zip_name, path], stdout=DEVNULL)
+
+subprocess.run(["mv", zip_name, out])
+
+os.chdir(out)
+
+subprocess.run(["unzip", "temp.zip"], stdout=DEVNULL, input=b'y')
+subprocess.run("clear", shell=True)
+os.remove("temp.zip")
